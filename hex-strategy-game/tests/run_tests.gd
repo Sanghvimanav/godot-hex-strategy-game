8eff3a512c36fe50a328e89229506874d7e0b50c
Sanghvimanav <sanghvimanav96@gmail.com>
2026-08-31 15:32:01 -0700

    Run mixed-force rollout budget matrix

--- hex-strategy-game/tests/run_tests.gd
+++ hex-strategy-game/tests/run_tests.gd
@@ -23,6 +23,7 @@ func _ready() -> void:
 	var TestOpponentConditionedCounters = load("res://tests/test_opponent_conditioned_counters.gd") as GDScript
 	var TestOpponentResponseRetreat = load("res://tests/test_opponent_response_retreat.gd") as GDScript
 	var TestPureStateGameRollout = load("res://tests/test_pure_state_game_rollout.gd") as GDScript
+	var TestPureStateGameRolloutBudgetMatrix = load("res://tests/test_pure_state_game_rollout_budget_matrix.gd") as GDScript
 	var TestServerTurnExecutor = load("res://tests/test_server_turn_executor.gd") as GDScript
 	var TestUnifiedPipeline = load("res://tests/test_unified_pipeline.gd") as GDScript
 	var TestStunEffects = load("res://tests/test_stun_effects.gd") as GDScript
@@ -94,6 +95,10 @@ func _ready() -> void:
 	else:
 		_pass_count += 1
 	if not TestPureStateGameRollout.run_all(self):
+		_fail_count += 1
+	else:
+		_pass_count += 1
+	if not TestPureStateGameRolloutBudgetMatrix.run_all(self):
 		_fail_count += 1
 	else:
 		_pass_count += 1
