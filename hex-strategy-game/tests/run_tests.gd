a23c403e63da954ba4e0b2dc6f05f3dcf74aa2ca
Sanghvimanav <sanghvimanav96@gmail.com>
2026-08-31 06:26:10 -0700

    Register one-turn search tests

--- hex-strategy-game/tests/run_tests.gd
+++ hex-strategy-game/tests/run_tests.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	var TestPureStateLegalActions = load("res://tests/test_pure_state_legal_actions.gd") as GDScript
 	var TestPureStatePlans = load("res://tests/test_pure_state_plans.gd") as GDScript
 	var TestPureStateEvaluator = load("res://tests/test_pure_state_evaluator.gd") as GDScript
+	var TestPureStateOneTurnSearch = load("res://tests/test_pure_state_one_turn_search.gd") as GDScript
 	var TestServerTurnExecutor = load("res://tests/test_server_turn_executor.gd") as GDScript
 	var TestUnifiedPipeline = load("res://tests/test_unified_pipeline.gd") as GDScript
 	var TestStunEffects = load("res://tests/test_stun_effects.gd") as GDScript
@@ -59,6 +60,10 @@ func _ready() -> void:
 	else:
 		_pass_count += 1
 	if not TestPureStateEvaluator.run_all(self):
+		_fail_count += 1
+	else:
+		_pass_count += 1
+	if not TestPureStateOneTurnSearch.run_all(self):
 		_fail_count += 1
 	else:
 		_pass_count += 1
